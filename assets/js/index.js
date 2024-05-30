695fde102bb232c99d51e53d4d130b183b882d00
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-05-30 17:10:09 +0700

    update note js modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,12 @@
 const modal = document.querySelectorAll('.services_modal'),
       modalButton = document.querySelectorAll('.services__button'),
       modalClose = document.querySelectorAll('.services__modal-close')
-
+// Tạo biến activeModal để chứa class cần thêm vào
 let activeModal = (modalClick) =>{
     modal[modalClick].classList.add('active-modal')
 }
+// Duyệt qua biến modalButton chứa class .services__button để lắng nghe sự kiện click
+// Nếu được click  thì thêm activeModal vào
 modalButton.forEach((modalButton, i)=>{
     modalButton.addEventListener('click', ()=>{
         activeModal(i)
@@ -16,11 +18,15 @@ modalButton.forEach((modalButton, i)=>{
 let activeCloseModal = (modalClick) =>{
     modal[modalClick].classList.remove('active-modal')
 }
+// Duyệt qua biến modalClose chứa class .services__modal-close để lắng nghe sự kiện click
+// Nếu được click  thì xóa activeModal ra
 modalClose.forEach((modalClose, i)=>{
     modalClose.addEventListener('click', ()=>{
         activeCloseModal(i)
     })
 })
+
+
 ///**======================= SWIPER TESTIMONIAL  =========================== */
 
 ///**======================= SHOW SCROLL UP =========================== */
